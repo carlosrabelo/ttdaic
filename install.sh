ef49ba8e3addbd0653849c094cf612667e292f64
Carlos Rabelo <developer@carlosrabelo.com.br>
2024-11-03 15:32:00 -0400

    2024-11-03 15:32

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.cmake-tools
+code --install-extension golang.go
+code --install-extension vscjava.vscode-java-pack
+code --install-extension platformio.platformio-ide
+code --install-extension TabNine.tabnine-vscode
